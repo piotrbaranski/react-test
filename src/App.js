859d493848cc94cd7e1eb1bc59c28c4e73eb3c00
piotrbaranski <p.baranski@yahoo.pl>
2017-11-23 07:16:53 +0100

    Separate header and intro

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+            </header>
+        )
+    }
+}
+
+class Intro extends Component {
+    render() {
+        return (
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        )
+    }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Header />
+          <Intro />
       </div>
     );
   }
