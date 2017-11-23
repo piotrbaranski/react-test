b8d007a02e9d953148df6808aa6487a8820b2f2d
piotrbaranski <p.baranski@yahoo.pl>
2017-11-23 07:57:26 +0100

    Move components to separate files.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Intro from './Intro';
 
-class Header extends Component {
-    render() {
-        return (
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
-            </header>
-        )
-    }
-}
-
-class Intro extends Component {
-    render() {
-        return (
-            <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-        )
-    }
-}
 
 class App extends Component {
   render() {
---
+++ src/Intro.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Intro.css'
+
+class Intro extends Component {
+    render() {
+        return (
+            <p className="Intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        )
+    }
+}
+
+export default Intro
